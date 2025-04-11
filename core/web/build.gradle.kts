f8244b61cbbc712b7bbac6c06c4d1ee0208708a2
Daniel <daniel.zappold@gmx.de>
2025-04-11 12:31:08 +0200

    ⬆️ update dependencies

--- core/web/build.gradle.kts
+++ core/web/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     implementation(project(":core:domain"))
 
     implementation(libs.bundles.http4k)
-    implementation("com.fasterxml.jackson.module:jackson-module-blackbird:2.17.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-afterburner:2.17.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-blackbird:2.18.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-afterburner:2.18.3")
 
     testFixturesApi(libs.bundles.http4k.testing)
 }
